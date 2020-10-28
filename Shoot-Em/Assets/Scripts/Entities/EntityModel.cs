10bafb2f3343ed6a52ce02166c3c034428d558ea
Ramax22 <ramagarbagna@outlook.com>
2020-10-28 19:55:31 -0300

    +player movement and look

---
+++ Shoot-Em/Assets/Scripts/Entities/EntityModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityModel : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    //FUNCIONES DE ACCIONES
+    public void Move(Vector3 dir)
+    {
+        var moveVelocity = dir * _speed;
+        _rb.velocity = moveVelocity;
+    }
+
+    public void Look(Vector3 point)
+    {
+        transform.LookAt(new Vector3(point.x, transform.position.y, point.z));
+    }
+}
